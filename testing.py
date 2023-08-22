3e67c8521623438b17d192d58e58fd077053911a
Alphabeovarc <113485340+Alphabeovarc@users.noreply.github.com>
2023-08-22 09:20:38 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,4 +6,4 @@ import plotly as pt
 st.write('testing app')
 st.file_uploader('upload', 'csv', True, 'uploaded_files')
 
-st.write(st.session_state.uploaded_files
+st.write(st.session_state.uploaded_files)
