8b1baac59b1240b406f5b9d05de69f5f5bac244f
Alphabeovarc <113485340+Alphabeovarc@users.noreply.github.com>
2023-08-22 14:51:58 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import plotly as pt
 
+df = pd.DataFrame()
+
 st.write('testing app')
-st.file_uploader('upload', 'csv', True, 'uploaded_files')
+st.file_uploader('upload', 'csv', False, 'uploaded_files')
 
 st.write(st.session_state.uploaded_files)
 
-if st.session_state.uploaded_files is not None:
-  df = pd.read_csv(st.session_state.uploaded_files)
-  st.dataframe(df)
+
+df = pd.read_csv(st.session_state.uploaded_files)
+st.dataframe(df)
   
