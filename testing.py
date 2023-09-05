48df3784a755ff8d9201b6634810b440ead41e6e
Alphabeovarc <113485340+Alphabeovarc@users.noreply.github.com>
2023-09-05 09:18:23 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -12,7 +12,9 @@ st.file_uploader('upload', 'csv', True, 'uploaded_files')
 st.write(st.session_state.uploaded_files)
 
 df = []
-df.append(pd.read_csv(st.session_state.uploaded_files, on_bad_lines = 'skip'))
+for file in st.session_state.uploaded_files:
+  df.append(pd.read_csv(st.session_state.uploaded_files, on_bad_lines = 'skip'))
 df = pd.concat(df, ignore_index = True)
+
 st.dataframe(df)
 st.write(df.memory_usage())
